e9dc5cbde54d67b438d9a58a667c4e4ce075fad7
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-03-28 19:23:23 +0330

    create 'search' in home

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,32 +1,86 @@
 import EducationalGroupsHome from "../../component/educationalGroupsHome";
+import Arrow from "@/component/icons/Arrow";
+import Search from "@/component/icons/Search";
 const Home = () => {
   return(
-      <div className='h-full w-screen flex flex-col bg-white '>
-          <div id="whyUniPoll" className='text-black flex flex-row items-center'>
-              <div className='basis-1/2 flex flex-col pl-72' dir='rtl'>
-                  <h3 className='text-5xl font-bold text-darkBlue items-center mb-12'>چرا UniPoll ؟</h3>
-                  <h3 className='text-3xl font-bold text-black items-center'> امکان نظر دهی به اساتید</h3>
-                  <p className='text-[1.75rem]  text-black items-center mb-8'>با امتیاز دهی و ارسال نظر ،به اساتید در
-                      بهبود عملکردشان کمک کنید.</p>
-                  <h3 className='text-3xl font-bold text-black items-center'> تسهیل گزینش اساتید</h3>
-                  <p className='text-[1.75rem]  text-black items-center mb-8'>سامانه به گونه ای طراحی شده است که
-                      دانشجویان هنگام انتخاب واحد بهترین دروس را انتخاب کنند.</p>
-                  <h3 className='text-3xl font-bold text-black items-center'> تسریع دست یابی به جزوه</h3>
-                  <p className='text-[1.75rem]  text-black items-center mb-8'>شما به اسانی میتوانید جزوات درس مورد نظر خود را پیدا کنید.</p>
-              </div>
-              <div className='basis-1/2'><img src='/images/why.jpg' alt={'logo'}/></div>
-          </div>
-          <div id="educationalGroups " className="flex flex-col items-center justify-center my-24">
-              <h3 className='text-5xl font-bold text-black items-center'>گروه های آموزشی دانشکده</h3>
-              <div className=' flex flex-row items-center my-24 space-x-16 '>
-                  <EducationalGroupsHome name='نرم افزار' text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است ....'/>
-                  <EducationalGroupsHome name='هوش مصنوعی' text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است....'mt='mt-28'/>
-                  <EducationalGroupsHome name='سخت افزار' text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است....'/>
-                  <EducationalGroupsHome name='شبکه ' text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است ....' mt='mt-28'/>
+      <div className='h-full w-screen flex flex-col bg-white space-y-16 '>
+          <div id="searchBar " className="flex flex-col items-center justify-items-center " dir="rtl">
+              <div className={"min-h-[35rem]  w-screen bg-[url('/images/searchbar.png')] bg-no-repeat bg-cover justify-items-center "}>
+                  <div className='pr-56 my-20'>
+                      <img src='/images/logo3.png' alt={'logo'}/>
+                      <h2 className='text-3xl  text-white'>فضایی برای کسب اطلاعات در مورد دروس و اساتید دانشکده</h2>
+                  </div>
+                  <form className='px-24 flex flex-row'>
+                      <div
+                          className='basis-1/4  flex flex-row h-14 w-80 rounded-3xl bg-white text-black  items-center justify-items-center '
+                          dir="rtl">
+                          <h5 className='text-xl font-bold text-black px-8'>جستجو:</h5>
+                          <div>
+                              <label className="inline-flex items-center">
+                                  <input type="radio" value="lessons"
+                                         className="form-radio h-5 w-5 text-blue-600 ml-1 "
+                                         name="options" required/>
+                                  <span className="ml-2 text-xl text-black">دروس</span>
+                              </label>
+
+                              <label className="inline-flex items-center">
+                                  <input type="radio" value="professor"
+                                         className="form-radio h-5 w-5 text-blue-600 checked:bg-yellow-400 ml-1 mr-2"
+                                         name="options"  required/>
+                                  <span className="ml-2 text-xl text-black">اساتید</span>
+                              </label>
+                          </div>
+                      </div>
+                      <div
+                          className='basis-3/4  flex flex-row h-14 w-80 rounded-3xl bg-white text-black  items-center justify-items-center mr-16'
+                          dir="rtl">
+                          <input type="search" id="default-search" className="block h-14 w-11/12 p-4 ps-10  text-black-900 rounded-3xl  text-black text-xl placeholder:text-[#8B8C8D]
+                             focus:outline-none  focus:ring-0 "
+                                 placeholder=" جستجو را شروع کن ....."/>
+                          <button type="submit"
+                                  className="px-4">
+                             <Search/>
+                          </button>
+                      </div>
+
+
+                  </form>
 
               </div>
           </div>
-</div>
-);
+          <div id="whyUniPoll" className=' w-screen text-black flex flex-row items-center'>
+              <div className='basis-1/2 flex flex-col pl-72 ' dir='rtl'>
+                  <h3 className='text-5xl font-bold text-darkBlue  mb-12'>چرا UniPoll ؟</h3>
+                  <h3 className='text-3xl font-bold text-black '> امکان نظر دهی به اساتید</h3>
+                  <p className='text-[1.75rem]  text-black items-center mb-8'>با امتیاز دهی و ارسال نظر ،به اساتید در
+                      بهبود عملکردشان کمک کنید.</p>
+                  <h3 className='text-3xl font-bold text-black '> تسهیل گزینش اساتید</h3>
+                  <p className='text-[1.75rem]  text-black  mb-8'>سامانه به گونه ای طراحی شده است که
+                      دانشجویان هنگام انتخاب واحد بهترین دروس را انتخاب کنند.</p>
+                  <h3 className='text-3xl font-bold text-black '> تسریع دست یابی به جزوه</h3>
+                  <p className='text-[1.75rem]  text-black mb-8'>شما به اسانی میتوانید جزوات درس مورد نظر
+                      خود را پیدا کنید.</p>
+              </div>
+              <div className='flex basis-1/2'><img src='/images/why.jpg' alt={'why UniPoll ?'}/></div>
+          </div>
+          <div id="educationalGroups " className=" w-screen flex flex-col items-center justify-center ">
+              <h3 className='text-5xl font-bold text-black items-center '>گروه های آموزشی دانشکده</h3>
+              <div className=' flex flex-row items-center my-24 space-x-16 '>
+                  <EducationalGroupsHome name='نرم افزار'
+                                         text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است ....'/>
+                  <EducationalGroupsHome name='هوش مصنوعی'
+                                         text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است....'
+                                         mt='mt-28'/>
+                  <EducationalGroupsHome name='سخت افزار'
+                                         text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است....'/>
+                  <EducationalGroupsHome name='شبکه '
+                                         text='گروه مهندسی نرم‌افزار دانشگاه اصفهان همواره به عنوان یکی از باسابقه‌ترین گروه‌های کامپیوتری در سطح کشور شناخته می‌شده است ....'
+                                         mt='mt-28'/>
+
+              </div>
+          </div>
+
+      </div>
+  );
 };
 export default Home;
--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -2,7 +2,7 @@ import Telegram from "@/component/icons/Telegram";
 import Phone from "@/component/icons/Phone";
 import Mail from "@/component/icons/Mail";
 import {useState} from "react";
-import  {useRouter}  from 'next/router';
+import {useRouter} from 'next/router';
 import Logo from "@/component/icons/Logo";
 import Arrow from "@/component/icons/Arrow";
 
@@ -10,8 +10,8 @@ const Login = () => {
     const router = useRouter();
 
     const [formValue, setFormValue] = useState({userName: "", password: ""});
-    let [login , setLogin]=useState(false);
-    const formSubmitHandler = async (e) =>{
+    let [login, setLogin] = useState(false);
+    const formSubmitHandler = async (e) => {
         e.preventDefault();
         console.log(formValue)
         try {
@@ -19,7 +19,7 @@ const Login = () => {
             const response = await fetch("http://localhost:8090/unipoll/v1/login", {
                 method: "GET",
                 headers: {
-                    'Authorization': `Basic ${ btoa(`${formValue.userName}:${formValue.password}`)}`,
+                    'Authorization': `Basic ${btoa(`${formValue.userName}:${formValue.password}`)}`,
                     "Content-Type": "application/json"
                 },
             });
@@ -28,13 +28,12 @@ const Login = () => {
             if (response.ok) {
                 setLogin(false);
                 console.log("OK");
-                localStorage.setItem('jwtToken',response.headers.get('Authorization'));
-                const jwtToken=localStorage.getItem('jwtToken');
+                localStorage.setItem('jwtToken', response.headers.get('Authorization'));
+                const jwtToken = localStorage.getItem('jwtToken');
                 console.log(jwtToken);
                 await router.push('/home');
                 //navigate("/home");
                 //redirect('/home');
-
 
 
             } else {
@@ -42,75 +41,78 @@ const Login = () => {
                 console.log("not ok")
             }
 
-        }catch (e) {
+        } catch (e) {
             console.error("we are GETTING ERROR", e)
         }
     }
 
-    return(
-    <div className={"h-screen w-screen flex flex-row bg-white"}>
+    return (
+        <div className={"h-screen w-screen flex flex-row bg-white"}>
 
-        <div className={" flex basis-4/12 bg-[url('/images/uni3.png')] bg-no-repeat bg-cover"} >
-            {/*<img src={"/images/uni.png"}*/}
-            {/*className={'w-full h-full backdrop-opacity-10 backdrop-invert'}*/}
-            {/* alt={"uni"}/>*/}
-        </div>
-        <div className={"flex flex-col items-center basis-8/12 bg-white justify-between mt-9 mb-4 px-5"}>
-            <div className={'flex flex-row self-start'}>
-                <Arrow/>
-                <a  href={'/home'} className='text-start text-darkBlue text-sm'>خانه</a>
+            <div className={" flex basis-4/12 bg-[url('/images/uni3.png')] bg-no-repeat bg-cover"}>
+                {/*<img src={"/images/uni.png"}*/}
+                {/*className={'w-full h-full backdrop-opacity-10 backdrop-invert'}*/}
+                {/* alt={"uni"}/>*/}
             </div>
+            <div className={"flex flex-col items-center basis-8/12 bg-white justify-between mt-9 mb-4 px-5"}>
+                <div className={'flex flex-row self-start'}>
+                    <Arrow/>
+                    <a href={'/home'} className='text-start text-darkBlue text-sm'>خانه</a>
+                </div>
 
-            <img src='/images/logo.png' alt={'logo'}/>
-            <p className={"text-base "}>با نام کاربری و رمز ورود گلستان خود وارد شوید</p>
-            <form style={{display: "flex", alignItems: "center", justifyContent: "center"} }
-                  onSubmit={formSubmitHandler}
-            className={"border-gray-700 flex flex-col align-items-center justify-between "}>
-                <div className={'flex flex-col w-80 mb-12'}>
-                    <p className={'text-xl pb-2 text-end pr-5'}> نام کاربری</p>
-                    <input
-                        name={"userName"}
-                        onChange={(event) => {
-                            setFormValue({...formValue, userName: event.target.value})
-                        }}
-                        className=' h-14 block bg-blue1 w-full border border-slate-300 rounded-2xl
+                <img src='/images/logo.png' alt={'logo'}/>
+                <p className={"text-base "}>با نام کاربری و رمز ورود گلستان خود وارد شوید</p>
+                <form style={{display: "flex", alignItems: "center", justifyContent: "center"}}
+                      onSubmit={formSubmitHandler}
+                      className={"border-gray-700 flex flex-col align-items-center justify-between "}>
+                    <div className={'flex flex-col w-80 mb-12'}>
+                        <p className={'text-xl pb-2 text-end pr-5'}> نام کاربری</p>
+                        <input
+                            name={"userName"}
+                            onChange={(event) => {
+                                setFormValue({...formValue, userName: event.target.value})
+                            }}
+                            className=' h-14 block bg-blue1 w-full border border-slate-300 rounded-2xl
                         pr-3 pl-3 shadow-lg focus:outline-none focus:border-slate-400 focus:ring-slate-400 focus:ring-1 '
-                        type="text"/>
-                </div>
-                <div className={'flex flex-col w-80'}>
-                    <p className={'text-xl pb-2 text-end pr-5'}>رمز ورود</p>
-                    <input
-                        name={"password"}
-                        onChange={(event) => {
-                            setFormValue({...formValue, password: event.target.value})
-                        }}
-                        className=' h-14 block bg-blue1 w-full border border-slate-300 rounded-2xl
+                            type="text"/>
+                    </div>
+                    <div className={'flex flex-col w-80'}>
+                        <p className={'text-xl pb-2 text-end pr-5'}>رمز ورود</p>
+                        <input
+                            name={"password"}
+                            onChange={(event) => {
+                                setFormValue({...formValue, password: event.target.value})
+                            }}
+                            className=' h-14 block bg-blue1 w-full border border-slate-300 rounded-2xl
                         pr-3 pl-3 shadow-lg focus:outline-none focus:border-slate-400 focus:ring-slate-400 focus:ring-1 '
-                        type="password"/>
-                </div>
-                <button className=' drop-shadow-md text-white text-center mt-20 rounded-3xl bg-darkBlue px-16 py-2 pb-3 shadow-lg shadow-gray-600 '
+                            type="password"/>
+                    </div>
+                    <button
+                        className=' drop-shadow-md text-white text-center mt-20 rounded-3xl bg-darkBlue px-16 py-2 pb-3 shadow-lg shadow-gray-600 '
                         type={"submit"}>
-                    ورود
-                </button>
-                {login && <p className={'text-red-700 text-[0.75rem] mt-2'}>
-                    .رمز ورود یا نام کاربری اشتباه است
-                </p>}
+                        ورود
+                    </button>
+                    {login && <p className={'text-red-700 text-[0.75rem] mt-2'}>
+                        .رمز ورود یا نام کاربری اشتباه است
+                    </p>}
 
-            </form>
-            <p className={"text-xs text-gray-500"}>
-                . ورود به سایت به منزله رعایت تمام
-                <span className='text-black'>
-                    <a  href={'/rules'} > مقررات </a>
+                </form>
+                <p className={"text-xs text-gray-500"}>
+                    . ورود به سایت به منزله رعایت تمام
+                    <span className='text-black'>
+                    <a href={'/rules'}> مقررات </a>
                 </span>
-                ذکر شده است
-            </p>
-            <div className={"flex flex-row gap-6"}>
-                <Telegram/>
-                <Phone/>
-                <Mail/>
-             </div>
+                    ذکر شده است
+                </p>
+                <div className={"flex flex-row gap-6"}>
+                    <Telegram/>
+                    <Phone/>
+                    <Mail/>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
+
+
 export default Login;
