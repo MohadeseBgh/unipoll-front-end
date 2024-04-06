c930c3c4095bb3ef5d61475458f8ceeb4cc9d421
SRB <saeedranjbar586@gmail.com>
2024-04-06 23:36:12 +0330

    update on lessonsSearch and make professorSearch

--- components/lessonsSearch/index.jsx
+++ components/lessonsSearch/index.jsx
@@ -4,32 +4,34 @@ import Star from "@/components/icons/Star";
 const LessonSearch = (props) => {
 
     return (
-        <div className="w-[85rem]  bg-opacity-15 bg-[#1FAAEA] max-h-60 rounded-3xl flex flex-row m-12 drop-shadow-2xl p-5" dir="rtl">
+        <div className="w-[85rem] bg-blend-normal shadow-2xl bg-opacity-15 bg-[#1FAAEA] max-h-60 rounded-3xl flex flex-row m-12 drop-shadow-2xl p-5" dir="rtl">
             <div className="basis-2/12 border-t-0 drop-shadow-xl ">
-                <img className={' h-full w-full rounded-3xl backdrop-contrast-50 scale-105 ' } src='/images/why.jpg' alt={'why UniPoll ?'} />
+                <img className={' h-full w-full rounded-3xl backdrop-contrast-50 scale-105 ' }
+                     src='/images/why.jpg' // آدرس گرفتن عکس هم فکر کنم باید از بک گرفته شود
+                     alt={'why UniPoll ?'} />
             </div>
             <div className="basis-8/12 px-3 flex flex-row gap-12 ">
-                <div className="flex flex-col gap-8 justify-center items-center">
+                <div className="flex flex-col gap-8 basis-4/12 justify-center items-center">
                     <div className={'flex flex-row'}>
-                        <p className="font-bold  text-3xl  font-bold text-black pl-8">نام درس:  </p>
+                        <p className="font-bold  text-3xl  font-bold text-black pl-4 pr-6">نام درس:  </p>
                         <p className="   text-2xl    ">{props.lessonsName}  برنامه نویسی </p>
                     </div>
                     <div className={'flex flex-row'}>
-                        <p className="font-bold text-3xl  font-bold text-black  ">استاد درس: {props.lessonsName}</p>
-                        <p className="    text-2xl      ">{props.lessonsName}  رمضانی </p>
+                        <p className="font-bold text-3xl  font-bold text-black pl-4 ">استاد درس: {props.lessonsName}</p>
+                        <p className="    text-2xl      ">{props.profssorName}  رمضانی </p>
                     </div>
 
                 </div>
-                 <div className={'flex flex-row'}>
+                 <div className={'flex flex-row pt-11 basis-7/12'}>
                     <p className="font-bold    text-3xl  font-bold text-black  ">توضیحات: {props.lessonsName}</p>
-                    <p className="  text-2xl   ">{props.lessonsName}  توضیحات درس است این جا </p>
+                    <p className="  text-2xl  pr-4 ">{props.descriptionn}  توضیحات درس است این جا است  </p>
 
                  </div>
 
             </div>
 
-            <div className="basis-2/12 ">
-                <div className='flex flex-row basis-1/4 ' >
+            <div className="basis-2/12 scale-125 py-7   pr-24 ">
+                <div className='flex flex-row basis-1/4 scale-150'  >
                     {props.rate >= 5 ? <YellowStar/> : <Star />}
                     {props.rate >= 4 ? <YellowStar/> : <Star/>}
                     {props.rate >= 3 ? <YellowStar/> : <Star/>}
---
+++ components/professorSearch/index.jsx
@@ -0,0 +1,38 @@
+
+const ProfessorSearch = (props) => {
+
+    return (
+        <div className="w-[85rem] bg-blend-normal shadow-2xl bg-opacity-15 bg-[#1FAAEA] max-h-60 rounded-3xl flex flex-row m-12 drop-shadow-2xl p-5" dir="rtl">
+            <div className="basis-2/12 border-t-0 drop-shadow-xl ">
+                <img className={' h-full w-full rounded-3xl backdrop-contrast-50 scale-105 ' }
+                     src='/images/why.jpg' // آدرس گرفتن عکس هم فکر کنم باید از بک گرفته شود
+                     alt={'why UniPoll ?'} />
+            </div>
+            <div className="basis-10/12 px-3 flex flex-row gap-12 ">
+                <div className="flex flex-col gap-8 justify-center items-center">
+                    <div className={'flex flex-row'}>
+                        <p className="font-bold  text-3xl  font-bold text-black pl-4 pr-4">نام استاد:  </p>
+                        <p className="   text-2xl    ">{props.professorNamee} حعفر </p>
+                    </div>
+                    <div className={'flex flex-row'}>
+                        <p className="font-bold text-3xl  font-bold text-black pl-4 pr-8">مرتبه علمی: </p>
+                        <p className="    text-2xl      ">{props.profssorDegree}  استادیار </p>
+                    </div>
+
+                </div>
+                <div className={"flex flex-col"}>
+                    <div className={'flex flex-row pt-11'}>
+                        <p className="font-bold    text-3xl  font-bold text-black  "> ایمیل: </p>
+                        <a href={'#'} className=" pt-1 text-2xl  pr-4 ">{props.professorEmail} saeedranjbar586@gmail.com </a>
+                    </div>
+                     <div className={'flex flex-row pt-8'}>
+                         <p className="font-bold    text-3xl  font-bold text-black  ">صفحه شخصی:</p>
+                        <a    href={'#'} className=" pt-1 text-2xl  pr-4 ">{props.professorSite} www.mio.com   </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfessorSearch;
