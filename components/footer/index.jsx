b1bb3ca24d02500e858a5e0d2a603b18f9177e60
SRB <saeedranjbar586@gmail.com>
2024-04-11 20:49:33 +0330

    logo update color text update Rtl footer update make component professorInformation

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,36 +1,54 @@
+import Polygon from "@/components/icons/polygon";
+import Phone from "@/components/icons/Phone";
+import BluePhone from "@/components/icons/BluePhone";
+import BlueTelegram from "@/components/icons/BlueTelegram";
+import Mail from "@/components/icons/Mail";
+import Mail2 from "@/components/icons/Mail2";
+
+
 const Footer = () => {
   return(
 
-      <div className={'h-96 w-full bg-gradient-to-r from-cyan-900 to-blue-900  flex flex-row '}>
+      <div className={'h-96 w-full bg-gradient-to-r from-cyan-900 to-blue-900  flex flex-row '} dir={"rtl"}>
           <div className={' text-2xl  text-white basis-5/12  flex flex-col pt-8'} >
-              <div className={'flex pr-12     '}>
+              <div className={'flex pr-12      '}>
                   <img src={'/images/logo3.png'}/>
               </div>
           <div className={'p-0 8 text-center'}>
-              <p className={'p-3 mr-2 '}>یونیپول سامانه انلاین جامع نظردهی اساتید دانشگاهی و اشتراک گذاری جزوات به صورت متمرکز است .
+              <p className={'p-3 mr-2 text-white  '}>یونیپول سامانه انلاین جامع نظردهی اساتید دانشگاهی و اشتراک گذاری جزوات به صورت متمرکز است .
                   هدف از ساخت این سامانه  کمک به دانشجویان
                   و اساتید در روند های اموزشی است  </p>
 
           </div>
           </div>
         <div className={' text-2xl  text-white basis-4/12  flex flex-col pt-8 pr-16'}>
-          <p className={'font-bold text-2xl '} >پیوند های مهم</p>
+          <p className={'font-bold text-2xl text-white'} >پیوند های مهم</p>
               <div className={'mt-6 mr-3'}>
                   <div className={'flex flex-row'}>
-                      <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                      <a href={'#'} className={'pd-3 block'}>خانه</a>
+                      <div className={'pl-2 pt-2'}>
+                          <Polygon/>
+                      </div>
+                      <a href={'#'} className={'pd-3 text-white block'}>خانه</a>
                   </div>
                   <div className={'flex flex-row pt-6'}>
-                      <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                      <a href={'#'} className={' block '}>گروه آموزشی</a>
+                      <div className={'pl-2 pt-2'}>
+                          <Polygon/>
+                      </div>
+                      <a href={'#'} className={' block text-white '}>گروه آموزشی</a>
+                  </div>
+                  <div className={'flex flex-row pt-6 items-center '}>
+                      <div className={'pl-2 pt-2'}>
+                          <Polygon/>
+                      </div>
+
+
+                      <a href={'#'} className={' block text-white  '}>جزوه های من</a>
                   </div>
                   <div className={'flex flex-row pt-6'}>
-                      <img className={'size-4 mt-3 ml-2 '} src={'/favicon.ico'}/>
-                      <a href={'#'} className={' block '}>جزوه های من</a>
-                  </div>
-                  <div className={'flex flex-row pt-6'}>
-                      <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                      <a href={'#'} className={' block'}>انجمن ها</a>
+                      <div className={'pl-2 pt-2'}>
+                          <Polygon/>
+                      </div>
+                      <a href={'#'} className={'text-white  block'}>انجمن ها</a>
                   </div>
 
                 <a  className={'p-3 block '}></a>
@@ -40,19 +58,25 @@ const Footer = () => {
 
         </div>
         <div className={' text-2xl  text-white basis-4/12  flex flex-col pt-8'}>
-          <p className={'font-bold text-2xl '} >ارتباط با ما </p>
+          <p className={'font-bold text-2xl text-white'} >ارتباط با ما </p>
           <div className={'pt-8'}>
               <div className={'flex flex-row pt-6'}>
-                  <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                  <a href={'#'} className={' block'}>تلفن: 091300000</a>
+                  <div className={'justify-center pt-1 pl-3'}>
+                      <BluePhone h={'25'} w={'25'}  />
+                  </div>
+                  <a href={'#'} className={'text-white block'}>تلفن: 091300000</a>
               </div>
               <div className={'flex flex-row pt-6'}>
-                  <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                  <a href={'#'} className={' block'}> تلگرام:@unipoll</a>
+                  <div className={'justify-center pt-1 pl-3'}>
+                      <BlueTelegram h={'25'} w={'25'}  />
+                  </div>
+                  <a href={'#'} className={'text-white block'}> تلگرام:@unipoll</a>
               </div>
               <div className={'flex flex-row pt-6'}>
-                  <img className={'size-4 mt-3 ml-2  '} src={'/favicon.ico'}/>
-                  <a href={'#'} className={' block'}>ایمیل: unipoll@gmail.com</a>
+                  <div className={'justify-center pt-2 pl-2'}>
+                      <Mail2/>
+                  </div>
+                  <a href={'#'} className={'text-white block'}>ایمیل: unipoll@gmail.com</a>
               </div>
           </div>
 
---
+++ components/icons/polygon.jsx
@@ -0,0 +1,8 @@
+const Polygon = () => {
+    return(
+        <svg width="13" height="16" viewBox="0 0 13 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.010534 8.50067L12.7174 0.979601L12.9034 15.7009L0.010534 8.50067Z" fill="#102C51"/>
+        </svg>
+    )
+}
+export default Polygon;
