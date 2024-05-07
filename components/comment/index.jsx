e10bb731e4a4aadc38986e33cdd283463746a81c
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-05-07 22:52:00 +0330

    add name and role to  comment

--- components/comment/index.jsx
+++ components/comment/index.jsx
@@ -1,9 +1,26 @@
+import CommentProfile from "@/components/icons/CommentProfile";
 
 const Comment = (props) => {
     return (
-        <div className='flex flex-row bg-[#E0F1FD] h-44 w-full shadow-lg rounded-lg'>
-            <div className='basis-4/12'></div>
-            <div className='basis-8/12'></div>
+        <div className='flex flex-row bg-[#E0F1FD] h-44 w-full shadow-lg rounded-lg p-4' dir='rtl'>
+            <div className='basis-2/12 flex items-center justify-center  '><CommentProfile/></div>
+            <div className='basis-10/12 flex flex-col'>
+                <div className='basis-1/4 flex flex-row'>
+                    <div className='basis-2/8 flex flex-row'>
+                        <div>
+                            <h4 className='text-black font-bold text-2xl'>{props.name}</h4>
+                            <p className='text-[#4D4A4A] text-lg mt-1'>{props.term}</p>
+                        </div>
+                        <div className='bg-[#4D4A4A] font-bold h-8 px-5  rounded-xl mt-1'>
+                            <p>{props.role}</p>
+                        </div>
+                    </div>
+                    <div className='basis-6/8' >
+
+                    </div>
+                </div>
+                <div className='basis-3/4'></div>
+            </div>
 
         </div>
     );
--- components/comments/index.jsx
+++ components/comments/index.jsx
@@ -3,7 +3,7 @@ import Comment from "@/components/comment";
 const Comments = (props) => {
     return (
         <div
-            className="flex flex-col  mx-60 gap-20 my-20 ">
+            className="flex flex-col  mx-60 gap-12 my-20 ">
             {props.comment.map(
                 (p, index) => <Comment key={index} name={p.name}
                                               text={p.text}
