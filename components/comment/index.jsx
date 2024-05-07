9f26961d76694700e94db79929ce07c1a27785e9
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-05-07 21:55:40 +0330

    create components

---
+++ components/comment/index.jsx
@@ -0,0 +1,12 @@
+
+const Comment = (props) => {
+    return (
+        <div className='flex flex-row bg-[#E0F1FD] h-44 w-full shadow-lg rounded-lg'>
+            <div className='basis-4/12'></div>
+            <div className='basis-8/12'></div>
+
+        </div>
+    );
+
+}
+export default Comment;
---
+++ components/comments/index.jsx
@@ -0,0 +1,16 @@
+import Comment from "@/components/comment";
+
+const Comments = (props) => {
+    return (
+        <div
+            className="flex flex-col  mx-60 gap-20 my-20 ">
+            {props.comment.map(
+                (p, index) => <Comment key={index} name={p.name}
+                                              text={p.text}
+                                              date={p.date} term={p.term} role={p.role}/>
+            )}
+        </div>
+    );
+
+}
+export default Comments;
