457ebf2208a7dff338c217a13634d898f389c841
SRB <saeedranjbar586@gmail.com>
2024-05-08 23:52:58 +0330

    groups page main structure

---
+++ pages/groups/index.jsx
@@ -0,0 +1,131 @@
+const Groups  = () => {
+return (
+    <div className={"h-screen w-screen flex flex-col bg-white text-black"} >
+        <div className={" flex flex-row   text-black font-bold text-3xl drop-shadow-xl place-content-center mt-16 "}>
+            انجمن ها
+        </div>
+        <div className={"mt-12 "} dir="rtl">
+            <div className={" flex flex-row gap-12 my-12  w-2/8 h-64 bg-opacity-15 bg-[#1FAAEA] bg-opacity-15 shadow-2xl rounded-3xl  "}>
+
+                <div className={"w-1/2 flex flex-col  "}>
+                    <div className={"h-2/8 text-black font-bold text-2xl m-3"}>
+                        <p>انجمن علمی</p>
+                    </div>
+                    <div className={"h-6/8 text-black font-bold text-1xl m-4"}>
+                        <p>انجمن علمی دانشکده مهندسی کامپیوتر دانشگاه اصفهان از هغت عضو اصلی و دبیر و نائب دبیر و مشاور انجمن تشکیل شده است. اصلی ترین برنامه های اجرایی دانشکده توسط انجمن علمی صورت میگیرد. برای ارتباط با ما میتوانید در کانال های تلگرام ما را پیدا کنید.</p>
+                    </div>
+                </div>
+
+                <div className={"w-1/2 "}>
+
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                </div>
+
+
+            </div>
+
+        </div>
+        <div className={"mt-"} dir="rtl">
+            <div className={" flex flex-row gap-12 my-12  w-2/8 h-64 bg-opacity-15 bg-[#1FAAEA] bg-opacity-15 shadow-2xl rounded-3xl  "}>
+
+                <div className={"w-1/2 flex flex-col  "}>
+                    <div className={"h-2/8 text-black font-bold text-2xl m-3"}>
+                        <p>انجمن علمی</p>
+                    </div>
+                    <div className={"h-6/8 text-black font-bold text-1xl m-4"}>
+                        <p>انجمن علمی دانشکده مهندسی کامپیوتر دانشگاه اصفهان از هغت عضو اصلی و دبیر و نائب دبیر و مشاور انجمن تشکیل شده است. اصلی ترین برنامه های اجرایی دانشکده توسط انجمن علمی صورت میگیرد. برای ارتباط با ما میتوانید در کانال های تلگرام ما را پیدا کنید.</p>
+                    </div>
+                </div>
+
+                <div className={"w-1/2 "}>
+
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                </div>
+
+
+            </div>
+
+        </div>
+        <div className={"mt-"} dir="rtl">
+            <div className={" flex flex-row gap-12 my-12  w-2/8 h-64 bg-opacity-15 bg-[#1FAAEA] bg-opacity-15 shadow-2xl rounded-3xl  "}>
+
+                <div className={"w-1/2 flex flex-col  "}>
+                    <div className={"h-2/8 text-black font-bold text-2xl m-3"}>
+                        <p>انجمن علمی</p>
+                    </div>
+                    <div className={"h-6/8 text-black font-bold text-1xl m-4"}>
+                        <p>انجمن علمی دانشکده مهندسی کامپیوتر دانشگاه اصفهان از هغت عضو اصلی و دبیر و نائب دبیر و مشاور انجمن تشکیل شده است. اصلی ترین برنامه های اجرایی دانشکده توسط انجمن علمی صورت میگیرد. برای ارتباط با ما میتوانید در کانال های تلگرام ما را پیدا کنید.</p>
+                    </div>
+                </div>
+
+                <div className={"w-1/2 "}>
+
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                </div>
+
+
+            </div>
+
+        </div>
+        <div className={"mt-"} dir="rtl">
+            <div className={" flex flex-row gap-12 my-12  w-2/8 h-64 bg-opacity-15 bg-[#1FAAEA] bg-opacity-15 shadow-2xl rounded-3xl  "}>
+
+                <div className={"w-1/2 flex flex-col  "}>
+                    <div className={"h-2/8 text-black font-bold text-2xl m-3"}>
+                        <p>انجمن علمی</p>
+                    </div>
+                    <div className={"h-6/8 text-black font-bold text-1xl m-4"}>
+                        <p>انجمن علمی دانشکده مهندسی کامپیوتر دانشگاه اصفهان از هغت عضو اصلی و دبیر و نائب دبیر و مشاور انجمن تشکیل شده است. اصلی ترین برنامه های اجرایی دانشکده توسط انجمن علمی صورت میگیرد. برای ارتباط با ما میتوانید در کانال های تلگرام ما را پیدا کنید.</p>
+                    </div>
+                </div>
+
+                <div className={"w-1/2 "}>
+
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                    <p>mio</p>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+
+
+        <div className="w-8/12 flex flex-row h-full ">
+        </div>
+        </div>
+
+
+    );
+}
+ export default Groups;
+/*overflow-y-auto h-screen transition-all duration-5 animate-loop-scroll ease-in-out*/
