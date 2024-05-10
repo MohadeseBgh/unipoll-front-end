0b71544a76f5b1a5e0e2338c6ac6c31a68ab1d15
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-05-10 22:37:24 +0330

    error handeler

---
+++ components/icons/ErorrIcon.jsx
@@ -0,0 +1,10 @@
+const ErorrIcon = () => {
+    return (<svg viewBox="0 0 24 24" className="w-9 h-9 text-red-500 stroke-current" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M12 8V12V8ZM12 16H12.01H12ZM21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    )
+}
+export default ErorrIcon
