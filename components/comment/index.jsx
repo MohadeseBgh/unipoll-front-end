3387c39c2e868221530cff97af4dcdbc23bde1bd
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-05-12 23:13:14 +0330

    ctd show comment

--- components/comment/index.jsx
+++ components/comment/index.jsx
@@ -3,26 +3,26 @@ import Report from "@/components/icons/Report";
 
 const Comment = (props) => {
     return (
-        <div className='flex flex-row bg-[#E0F1FD] min-h-44 w-full shadow-lg rounded-lg p-4' dir='rtl'>
+        <div className='flex flex-row bg-[#E0F1FD] min-h-36 w-full shadow-lg rounded-lg p-4' dir='rtl'>
             <div className='basis-2/12 flex items-center justify-center  '><CommentProfile/></div>
             <div className='basis-10/12 flex flex-col'>
                 <div className='basis-1/4 flex flex-row justify-between'>
                     <div className='basis-2/8 flex flex-row'>
                         <div>
-                            <h4 className='text-black font-bold text-2xl'>{props.name}</h4>
+                            <h4 className='text-black font-bold text-xl'>{props.name}</h4>
                             <p className='text-[#4D4A4A] text-lg mt-1'>{props.term}</p>
                         </div>
-                        <div className='bg-[#4D4A4A] font-bold h-8 px-5  rounded-xl mt-1'>
+                        <div className='bg-[#4D4A4A] flex items-center text-center text-sm font-bold h-6 px-5  rounded-xl mt-1 mr-2'>
                             <p>{props.role}</p>
                         </div>
                     </div>
                     <div className='basis-6/8 flex flex-row' >
-                        <p className='text-[#4D4A4A] text-lg mt-1 ml-2'>{props.date}</p>
+                        <p className='text-[#4D4A4A] text-sm mt-1 ml-2'>{props.date}</p>
                         <Report/>
                     </div>
                 </div>
                 <div className='basis-3/4'>
-                    <p className='text-darkBlue text-xl mt-1 ml-2'>{props.text}</p>
+                    <p className='text-darkBlue text-lg mt-1 ml-2'>{props.text}</p>
                 </div>
             </div>
 
--- components/comments/index.jsx
+++ components/comments/index.jsx
@@ -1,14 +1,111 @@
 import Comment from "@/components/comment";
+import {useContext, useEffect, useState} from "react";
+import {coursePIDContext} from "@/context/coursePIDContext";
 
-const Comments = (props) => {
+const Comments = () => {
+    const [comments, setComments] = useState([]);
+    const [selectedCourse , setSelectedCourse]=useContext(coursePIDContext);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8090/unipoll/v1/comment-c/${selectedCourse.publicId}?filterTopFive=`+true ,{
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                if (response.ok) {
+                    console.log("OK");
+                    const data=await response.json();
+                    const result=data.result
+                    console.log(result)
+                    setComments(result)
+                } else {
+                    console.log("not ok")
+                }
+
+            }catch (e) {
+                console.error("we are GETTING ERROR", e)
+            }
+        };
+
+        fetchData();
+    }, []);
+    const moreHandler = async (e) => {
+        e.preventDefault();
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8090/unipoll/v1/comment-c/${selectedCourse.publicId}?filterTopFive=`+false ,{
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                if (response.ok) {
+                    console.log("OK");
+                    const data=await response.json();
+                    const result=data.result
+                    console.log(result)
+                    setComments(result)
+                    document.getElementById("moreCourse").className = ' hidden';
+                    document.getElementById("lessCourse").className = '  mt-3 flex justify-center';
+                } else {
+                    console.log("not ok")
+                }
+
+            }catch (e) {
+                console.error("we are GETTING ERROR", e)
+            }
+        };
+
+        fetchData();
+
+    }
+    const lessHandler = async (e) => {
+        e.preventDefault();
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8090/unipoll/v1/comment-c/${selectedCourse.publicId}?filterTopFive=`+true ,{
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                if (response.ok) {
+                    console.log("OK");
+                    const data=await response.json();
+                    const result=data.result
+                    console.log(result)
+                    setComments(result)
+                    document.getElementById("moreCourse").className = 'flex absolute bottom-0 left-0 right-0 h-1/4 bg-gradient-to-b from-transparent to-white items-end justify-center';
+                    document.getElementById("lessCourse").className = 'hidden';
+                } else {
+                    console.log("not ok")
+                }
+
+            }catch (e) {
+                console.error("we are GETTING ERROR", e)
+            }
+        };
+
+        fetchData();
+
+    }
     return (
-        <div
-            className="flex flex-col  mx-60 gap-12 my-20 ">
-            {props.comment.map(
-                (p, index) => <Comment key={index} name={p.name}
-                                              text={p.text}
-                                              date={p.date} term={p.term} role={p.role}/>
+        <div className="relative bg-white p-6 flex flex-col  mx-60 gap-12 my-20">
+            {comments.map(
+                (p, index) => <Comment key={index} name={p.writerName}
+                                       text={p.text}
+                                       date={p.createdDate} term={p.term} role={p.writerType}/>
             )}
+            <div id={'moreCourse'}
+                 className="flex absolute bottom-0 left-0 right-0 h-1/4 bg-gradient-to-b from-transparent to-white items-end justify-center">
+                <button onClick={moreHandler}><p className={" text-darkBlue text-xl   font-bold"}>مشاهده بیشتر</p></button>
+            </div>
+            <div id={'lessCourse'}
+                 className="hidden">
+                <button onClick={lessHandler}><p className={"text-darkBlue  text-xl font-bold"}> نمایش کمتر</p></button>
+            </div>
         </div>
     );
 
