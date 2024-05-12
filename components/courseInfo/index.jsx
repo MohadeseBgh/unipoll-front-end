fd3990ba4cc2231380219986806ed22fcaa68402
pacific <mhds.8200.b@gmail.com>
2024-05-12 21:23:46 +0330

    handle edit button

--- components/courseInfo/index.jsx
+++ components/courseInfo/index.jsx
@@ -6,6 +6,10 @@ import Edit from "@/components/icons/Edit";
 import {coursePIDContext} from "@/context/coursePIDContext";
 
 const CourseInfo = () => {
+    //
+    const [rating, setRating] = useState({"rate":0.0});
+    const [hover, setHover] = useState(null);
+    //
     const [edit , setEdit]=useState(false);
     const [editAccess , setEditAccess]=useState(false);
     const [selectedCourse , setSelectedCourse]=useContext(coursePIDContext);
@@ -15,13 +19,30 @@ const CourseInfo = () => {
         const fetchData = async () => {
             console.log('publicId:')
             console.log(selectedCourse)
+            const jwtToken=localStorage.getItem('jwtToken');
+
             try {
                 const response = await fetch(`http://localhost:8090/unipoll/v1/instructor-course/${selectedCourse.publicId}`);
 
                 if (response.ok) {
                     const data = await response.json();
                     setCourseInfo(data.result);
-                    setRate(courseInfo.rate)
+                    setRate( courseInfo.rate);
+                    console.log(data.result);
+                } else {
+                    console.log("Network response was not ok");
+                }
+                const response1 = await fetch(`http://localhost:8090/unipoll/v1/rate/${selectedCourse.publicId}`,{
+                        method: 'GET',
+                        headers: {
+                            'Authorization': jwtToken
+                        },
+                    });
+
+                if (response1.ok) {
+                    const data = await response1.json();
+                    //console.log(data.result)
+                    setRating(data.result)
 
                 } else {
                     console.log("Network response was not ok");
@@ -32,11 +53,40 @@ const CourseInfo = () => {
         };
 
         fetchData();
-    }, []);
+    }, [courseInfo.rate ]);
+    const handleRatingChange = (currentRating) => {
+        setRating({ "rate": currentRating });
+        handleRating().then(r => { });
+    };
     const editClick = (e) => {
       e.preventDefault();
       if (editAccess===true)
         setEdit(true);
+    }
+    const handleRating = async () => {
+        console.log(rating);
+        const jwtToken=localStorage.getItem('jwtToken');
+
+        try {
+            const response = await fetch(`http://localhost:8090/unipoll/v1/rate/${selectedCourse.publicId}`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': jwtToken,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(rating),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('ok');
+                console.log(data);
+
+            } else {
+                console.log("Not ok");
+            }
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
     }
     return(
       <div className={'w-full h-full bg-[#E2F4FC] shadow-[0_0_60px_-15px_rgba(0,0,0,0.3)] flex flex-col p-5 gap-5'}>
@@ -58,8 +108,47 @@ const CourseInfo = () => {
                   </div>
                   <ShowCourseInfo editButton={edit} courseName={courseInfo.courseName} rate={courseInfo.rate} courseProfessor={courseInfo.instructorCourseFirstname +' '+ courseInfo.instructorCourseLastname} vahed={courseInfo.unit} info={courseInfo.description}/>
                   <div className={'h-0.5 w-full bg-darkBlue '}></div>
-                  <div id={'rating'}>
-                        <p className={'text-lg text-black'}>امتیاز خودرا به این درس بدهید</p>
+                  <div id={'rating'} className={'flex flex-row justify-between items-center w-full'}>
+                      <p className={'text-lg text-black'}>امتیاز خودرا به این درس بدهید</p>
+                        <div dir={'ltr'}>
+                            {[...Array(5)].map((star, index) => {
+                                const currentRating = index + 1;
+                                return (
+                                    <label key={index} >
+                                        <input className={'hidden '}
+                                               type="radio"
+                                               name="rating"
+                                               value={currentRating}
+                                               onClick={() => handleRatingChange(currentRating)}
+                                        />
+                                        <span
+                                            className={`p-5 cursor-pointer text-3xl ${currentRating <= (hover || rating.rate) ? "text-[#ffc107]" : "text-white "}`}
+                                            onMouseEnter={() => setHover(currentRating)}
+                                            onMouseLeave={() => setHover(null)}
+                                        >&#9733;
+                                  </span>
+                                    </label>
+                                );
+                            })}
+                        </div>
+
+                      {/*<div id={'show_rate'} className={'flex flex-row-reverse items-center'}>*/}
+                      {/*    <div onClick={handleRating} >*/}
+                      {/*        {rating >= 1 ? <YellowStar/> : <Star/>}*/}
+                      {/*    </div>*/}
+                      {/*    <div>*/}
+                      {/*        {rating >= 2 ? <YellowStar/> : <Star/>}*/}
+                      {/*    </div>*/}
+                      {/*    <div>*/}
+                      {/*        {rating >= 3 ? <YellowStar/> : <Star/>}*/}
+                      {/*    </div>*/}
+                      {/*    <div>*/}
+                      {/*        {rating >= 4 ? <YellowStar/> : <Star/>}*/}
+                      {/*    </div>*/}
+                      {/*    <div>*/}
+                      {/*        {rating >= 5 ? <YellowStar/> : <Star/>}*/}
+                      {/*    </div>*/}
+                      {/*</div>*/}
                   </div>
               </div>
           </div>
