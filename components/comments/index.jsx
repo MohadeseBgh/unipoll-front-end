24f524a286a360587ad6dc3bd5f20b96b0943ec7
nastaranSarkhosh <sarkhoshnastaran@gmail.com>
2024-05-14 22:07:17 +0330

    filter ctb

--- components/comments/index.jsx
+++ components/comments/index.jsx
@@ -9,7 +9,7 @@ const Comments = () => {
     const [totalComments, setTotalComments] = useState([]);
     const [selectedCourse , setSelectedCourse]=useContext(coursePIDContext);
     const [filterList, setFilterList] = useState([]);
-    const [filter, setFilter] = useState([]);
+    const [filter, setFilter] = useState({publicId:""});
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -154,13 +154,51 @@ const Comments = () => {
         fetchData();
 
     }
+    const filterHandler = async (e) => {
+        e.preventDefault();
+        setFilter({publicId: e.target.value});
+        const fetchData = async () => {
+            try {
+                const response3 = await fetch(`http://localhost:8090/unipoll/v1/comment-c/${selectedCourse.publicId}?filterTopFive=`+false+`&term=${filter.publicId}` ,{
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                if (response3.ok) {
+                    console.log("filter OK");
+                    const data3=await response3.json();
+                    const result3=data3.result;
+                    const temp=result3;
+                    console.log(result3);
+                    setComments(result3);
+                    const temp2=comments;
+                    setComments(result3);
+                    document.getElementById("moreCourse").className = 'hidden';
+                    document.getElementById("lessCourse").className = 'hidden';
+                    document.getElementById("noComment").className = 'hidden';
+                    document.getElementById("deleteFilter").className = 'flex flex-col basis-1/4 items-end';
+                    if(result3.length===0){
+                        document.getElementById("noComment").className = 'text-darkBlue  text-2xl font-bold text-center my-10';
+                    }
+                } else {
+                    console.log("not ok")
+                }
+
+            }catch (e) {
+                console.error("we are GETTING ERROR", e)
+            }
+        };
+        fetchData();
+
+    }
     return (
         <div className="relative bg-white p-6 flex flex-col  mx-60 gap-12 my-20">
             <div className={"flex flex-row justify-between "}>
                 <div className='flex flex-col base-3/4 items-end text-black'>
                     <select
                         data-twe-select-placeholder="فیلتر بر اساس سر ترم"
-                        className='appearance-none  w-60 h-12 pr-4 bg-[#DFE8EF]  focus:border-0 shadow-inner rounded-lg focus:outline-0 placeholder:text-yellow-300  peer-active:text-yellow-600'>
+                        className='appearance-none  w-60 h-12 pr-4 bg-[#DFE8EF]  focus:border-0 shadow-inner rounded-lg focus:outline-0 placeholder:text-yellow-300  peer-active:text-yellow-600' onChange={filterHandler}>
                         <option value="" hidden selected
                                 className="checked:text-[#8B8C8D] peer-checked:text-yellow-300 text-2xl">فیلتر بر اساس  ترم
                         </option>
@@ -169,7 +207,7 @@ const Comments = () => {
                         )}
                     </select>
                 </div>
-                <div className='flex flex-col basis-1/4 items-end'>
+                <div id={"deleteFilter"} className='hidden'>
                     <button type="submit"
                             className=" flex flex-row  items-center text-center px-4 bg-darkBlue text-white font-extralight rounded-xl w-40 h-12 text-xl   hover:drop-shadow-xl hover:shadow-teal-950 pr-8 "><Close2/><p className={"mr-2"}>حذف فیلتر</p>
                     </button>
